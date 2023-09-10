8f4a160d1ed12cdf8a3bfa5bccf2b0cd3281bfd1
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-09-09 21:02:00 -0400

    feat: Add lemma REPL binary

--- lemma/README.md
+++ lemma/README.md
@@ -9,3 +9,12 @@ Lisp-dialect focused on providing uniform code and data interface for [vrsjmp](h
 - Embedding within Rust programs
 - Ease of native bindings
 - Serializable data structures and code
+
+## REPL
+
+There is a binary target that exposes a simple REPL:
+
+```sh
+# in vrsjmp root folder
+$ cargo run --bin lemma
+```
--- lemma/src/main.rs
+++ lemma/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello lemma");
+//! Main REPL for Lemma
+use std::error::Error;
+use std::io::{self, Write};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    loop {
+        print!("lemma> ");
+        io::stdout().flush()?;
+
+        io::stdin().read_line(&mut input)?;
+
+        match lemma::eval_expr(&input) {
+            Ok(val) => println!("{}", val),
+            Err(e) => println!("{}", e),
+        }
+
+        input.clear();
+    }
 }
