646a7e4ad05777d136fd262a7a2fffbf5d01b0a3
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-10-01 08:47:09 -0400

    feat: Add WeakProcessHandle

--- libvrs/src/runtime/process.rs
+++ libvrs/src/runtime/process.rs
@@ -7,15 +7,17 @@ use tokio::sync::{mpsc, oneshot};
 /// Spawn a new process
 pub(crate) fn spawn() -> ProcessHandle {
     let (msg_tx, mut msg_rx) = mpsc::channel(32);
+    let handle = ProcessHandle { msg_tx };
+    let weak_handle = handle.clone().downgrade();
     tokio::spawn(async move {
-        let mut proc = Process::new();
+        let mut proc = Process::new(weak_handle);
         while let Some(msg) = msg_rx.recv().await {
             proc.handle_msg(msg).await;
         }
         Ok::<(), Error>(())
     });
 
-    ProcessHandle { msg_tx }
+    handle
 }
 
 /// ID assigned to [Process]
@@ -23,9 +25,15 @@ pub(crate) fn spawn() -> ProcessHandle {
 pub(crate) struct ProcessId(usize);
 
 /// Handle to [Process]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub(crate) struct ProcessHandle {
     msg_tx: mpsc::Sender<Message>,
+}
+
+/// Weak version of [ProcessHandle]
+#[derive(Debug, Clone)]
+pub(crate) struct WeakProcessHandle {
+    msg_tx: mpsc::WeakSender<Message>,
 }
 
 impl ProcessHandle {
@@ -41,8 +49,25 @@ impl ProcessHandle {
         self.msg_tx.send(Message::Cast(form)).await?;
         Ok(())
     }
+
+    /// Downgrade this proces handle into weak process handle that does not keep process alive
+    pub(crate) fn downgrade(&self) -> WeakProcessHandle {
+        WeakProcessHandle {
+            msg_tx: self.msg_tx.downgrade(),
+        }
+    }
 }
 
+impl WeakProcessHandle {
+    /// Upgrade into process handle
+    pub(crate) fn upgrade(&self) -> Option<ProcessHandle> {
+        self.msg_tx
+            .upgrade()
+            .and_then(|msg_tx| Some(ProcessHandle { msg_tx }))
+    }
+}
+
+/// Messages that [Process] responds to
 pub enum Message {
     Call(lemma::Form, oneshot::Sender<Result<lemma::Form>>),
     Cast(lemma::Form),
@@ -52,12 +77,15 @@ pub enum Message {
 pub(crate) struct Process<'a> {
     /// Environment of interpreter
     env: lemma::Env<'a>,
+    /// The weak process handle that this process may handoff to external tasks
+    handle: WeakProcessHandle,
 }
 
 impl Process<'_> {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(handle: WeakProcessHandle) -> Self {
         Self {
             env: lemma::lang::std_env(),
+            handle,
         }
     }
 
@@ -122,6 +150,30 @@ mod tests {
         ));
     }
 
-    // TODO: Test that cast is nonblocking, even for long-running operations
-    // TODO: Test that killing process is not blocked by long-running operations
+    #[tokio::test]
+    async fn proc_weak_handle_upgrade() {
+        let proc = spawn();
+        let weak = proc.downgrade();
+
+        assert!(
+            weak.upgrade().is_some(),
+            "Upgrading weak handle before original proc handle was dropped should return Some"
+        );
+    }
+
+    #[tokio::test]
+    async fn proc_weak_handle_upgrade_after_drop() {
+        let proc = spawn();
+        let weak = proc.downgrade();
+        drop(proc);
+
+        assert!(
+            weak.upgrade().is_none(),
+            "Upgrading weak handle after original proc handle was dropped should return None"
+        );
+    }
+
+    // TODO: Test: that cast is nonblocking, even for long-running operations
+    // TODO: Test: that killing process is not blocked by long-running operations
+    // TODO: Test: Subscriptions are ignored / cancelled when process handle is dropped
 }
