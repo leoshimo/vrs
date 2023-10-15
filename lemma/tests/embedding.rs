48b8c918f8ee62447526f68e8fd9b6a5a3a26b06
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-10-15 15:53:31 -0400

    test: Embedding test cases for yielding programs

--- lemma/tests/embedding.rs
+++ lemma/tests/embedding.rs
@@ -34,12 +34,80 @@ fn fiber_invalid_bytecode() {
     assert_matches!(f.resume(), Err(Error::UnexpectedStack(_)));
 }
 
-// #[test]
-// fn fiber_yielding() {
-//     let mut f = Fiber::from_bytecode(vec![]); // TODO: Fill instructions
+#[test]
+fn fiber_yielding() {
+    // An infinitely increasing counter increasing by one each iteration
+    let prog = r#"
+        (begin 
+            (def x 0)
+            (defn yielding_add ()
+                (yield x)
+                (set x (+ x 1))
+                (yielding_add))
+            (yielding_add)
+        )
+    "#;
+    let mut f = Fiber::from_expr(prog).unwrap();
+    f.bind(lemma::NativeFn {
+        symbol: lemma::SymbolId::from("+"),
+        func: |x| match x {
+            [Val::Int(a), Val::Int(b)] => Ok(Val::Int(a + b)),
+            _ => panic!("only supports ints"),
+        },
+    });
 
-//     // TODO: Does match f.status() loop even work!? with mutable manipulations of f?
-//     f.start()
-// }
+    assert_eq!(f.resume().unwrap(), Yield(Val::Int(0)));
+    assert_eq!(f.resume_from_yield(Val::Nil).unwrap(), Yield(Val::Int(1)));
+    assert_eq!(f.resume_from_yield(Val::Nil).unwrap(), Yield(Val::Int(2)));
+    assert_eq!(f.resume_from_yield(Val::Nil).unwrap(), Yield(Val::Int(3)));
+    assert_eq!(f.resume_from_yield(Val::Nil).unwrap(), Yield(Val::Int(4)));
+    assert_eq!(f.resume_from_yield(Val::Nil).unwrap(), Yield(Val::Int(5)));
 
-// // TODO: Test error propagation when fiber is already running
+    // TODO: Use fiber_yielding as test case for tail-call optimization (shouldn't grow number of CallFrames)
+}
+
+#[test]
+fn fiber_yielding_by_arg() {
+    // An infinitely increasing counter increasing by yield-ed value
+    let prog = r#"
+        (begin 
+            (def x 0)
+            (defn yielding_add ()
+                (set x (+ x (yield x)))
+                (yielding_add))
+            (yielding_add)
+        )
+    "#;
+    let mut f = Fiber::from_expr(prog).unwrap();
+    f.bind(lemma::NativeFn {
+        symbol: lemma::SymbolId::from("+"),
+        func: |x| match x {
+            [Val::Int(a), Val::Int(b)] => Ok(Val::Int(a + b)),
+            _ => panic!("only supports ints"),
+        },
+    });
+
+    assert_eq!(f.resume().unwrap(), Yield(Val::Int(0)));
+    assert_eq!(
+        f.resume_from_yield(Val::Int(1)).unwrap(),
+        Yield(Val::Int(1))
+    );
+    assert_eq!(
+        f.resume_from_yield(Val::Int(2)).unwrap(),
+        Yield(Val::Int(3))
+    );
+    assert_eq!(
+        f.resume_from_yield(Val::Int(3)).unwrap(),
+        Yield(Val::Int(6))
+    );
+    assert_eq!(
+        f.resume_from_yield(Val::Int(4)).unwrap(),
+        Yield(Val::Int(10))
+    );
+    assert_eq!(
+        f.resume_from_yield(Val::Int(5)).unwrap(),
+        Yield(Val::Int(15))
+    );
+}
+
+// TODO: Test error propagation when fiber is already running
