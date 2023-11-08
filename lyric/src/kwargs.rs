e630fe0d07dcdf31523528e6e08be8b3a0b3feb0
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-11-08 09:55:46 -0800

    fix: Use slice instead of Vec

--- lyric/src/kwargs.rs
+++ lyric/src/kwargs.rs
@@ -3,8 +3,10 @@
 use crate::{Extern, KeywordId, Locals, Val};
 
 /// Get the value associated with `keyword` from `lst`, if any.
-pub fn get<T: Extern, L: Locals>(lst: &Vec<Val<T, L>>, target: &KeywordId) -> Option<Val<T, L>> {
+pub fn get<T: Extern, L: Locals>(lst: &[Val<T, L>], target: &KeywordId) -> Option<Val<T, L>> {
     lst.windows(2)
         .find(|w| matches!(&w[0], Val::Keyword(key) if key == target))
         .map(|w| w[1].clone())
 }
+
+// TODO: Tests for `kwargs::get`
