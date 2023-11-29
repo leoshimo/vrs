983115540fad49e8f97485b55fcac2bfa5fc100a
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-11-28 21:33:14 -0800

    feat: serve.sh shows logs

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -2,8 +2,9 @@
 # serve.sh - Trivial Serve
 
 while true; do
-    PID=$(RUST_LOG=debug cargo run --bin vrsd) &
+    PID=$(RUST_LOG=debug cargo run --bin vrsd > vrsd.log) &
     sleep 1
     ./scripts/launcher.ll &
+    tail -f vrsd.log
     wait $PID
 done
