9d0a0f123daff81651c146dfca5d50dfe968773b
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-11-30 00:28:40 -0800

    feat: Auto rename tmux window

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 # serve.sh - Trivial Serve
 
+if [ "$TMUX" ]; then
+    tmux rename-window "serve"
+fi
+
 while true; do
     PID=$(RUST_LOG=debug cargo run --bin vrsd > vrsd.log) &
     sleep 1
