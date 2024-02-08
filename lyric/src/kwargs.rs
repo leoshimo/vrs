677a238bdf166ca16b187aa055f90d25eb5f1cd1
leoshimo <56844000+leoshimo@users.noreply.github.com>
2024-02-08 10:38:57 -0800

    fix: kwargs::flag does not return non boolean values

--- lyric/src/kwargs.rs
+++ lyric/src/kwargs.rs
@@ -13,14 +13,18 @@ pub fn get<T: Extern, L: Locals>(lst: &[Val<T, L>], target: &KeywordId) -> Optio
 /// Supports both implicit and explicit values:
 /// - (my_func :my_flag)
 /// - (my_func :my_flag true) / (my_func :my_flag false)
+/// If value after `:my_flag` is not a boolean, it is not considered a value for flag argument
 pub fn flag<T: Extern, L: Locals>(lst: &[Val<T, L>], target: &KeywordId) -> Option<Val<T, L>> {
     let mut iter = lst
         .iter()
         .skip_while(|v| matches!(v, Val::Keyword(kwd) if kwd != target));
 
     if iter.next().is_some() {
-        let val = iter.next().unwrap_or(&Val::Bool(true)); // implicit true flag
-        Some(val.clone())
+        let val = match iter.next() {
+            Some(Val::Bool(b)) => Val::Bool(*b), // explicit flag
+            _ => Val::Bool(true),                // implicit true flag if missing or not boolean
+        };
+        Some(val)
     } else {
         None // no explicit arg
     }
