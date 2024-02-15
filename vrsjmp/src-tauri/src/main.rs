ac3c861be82cba052b5548596f26f7cc2bb2997a
leoshimo <56844000+leoshimo@users.noreply.github.com>
2024-02-14 22:18:25 -0800

    feat: vrsjmp windows are vibrant

--- vrsjmp/src-tauri/src/main.rs
+++ vrsjmp/src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use serde_json::json;
+use tauri::Manager;
+
+use window_vibrancy::NSVisualEffectState;
+#[cfg(target_os = "macos")]
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
 #[tauri::command]
 fn set_query(query: &str) -> Vec<serde_json::Value> {
@@ -21,6 +26,20 @@ fn dispatch(form: &str) {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(
+                &window,
+                NSVisualEffectMaterial::HudWindow,
+                Some(NSVisualEffectState::Active),
+                Some(16.0),
+            )
+            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![set_query, dispatch])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
