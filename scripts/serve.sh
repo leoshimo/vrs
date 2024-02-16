b2dfd2fd0cad75663d7f3eb2f0f3660f0ac7b946
leoshimo <56844000+leoshimo@users.noreply.github.com>
2024-02-16 13:37:31 -0800

    feat: serve.sh can run in both --debug and --release configurations

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
 # serve.sh - Trivial Serve
+#
+# For Dev:
+#     ./serve.sh
+# For Live-On Dev:
+#     ./serve.sh --release
 
 if [ "$TMUX" ]; then
     tmux rename-window "serve"
 fi
 
 while true; do
-    PID=$(RUST_LOG=debug cargo run --bin vrsd --release > vrsd.log) &
+    PID=$(RUST_LOG=debug cargo run --bin vrsd $@ > vrsd.log) &
 
     while true; do
-        ./scripts/launcher.ll >/dev/null 2>&1
+        cargo run --bin vrsctl $@ ./scripts/launcher.ll >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo "Successfully started launcher.ll"
             break
