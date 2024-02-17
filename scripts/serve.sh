5942befe0ddfe1949c0dedefa18a92f9e13ea4dc
leoshimo <56844000+leoshimo@users.noreply.github.com>
2024-02-16 16:59:23 -0800

    feat: QOL: Improve ergonomics of serve.sh with vrsjmp for live-on mode

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -2,26 +2,47 @@
 # serve.sh - Trivial Serve
 #
 # For Dev:
+#     ./serve.sh dev
+#
+# For Live-On:
 #     ./serve.sh
-# For Live-On Dev:
-#     ./serve.sh --release
 
-if [ "$TMUX" ]; then
-    tmux rename-window "serve"
+MODE="live-on"
+if [ "$1" = "dev" ]; then
+    MODE="dev"
 fi
 
-while true; do
-    PID=$(RUST_LOG=debug cargo run --bin vrsd $@ > vrsd.log) &
+if [ "$TMUX" ]; then
+    tmux rename-window "vrs-srv-$MODE"
+fi
 
-    while true; do
-        cargo run --bin vrsctl $@ ./scripts/launcher.ll >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            cargo run --bin vrsctl $@ ./scripts/chat.ll >/dev/null 2>&1
-            cargo run --bin vrsctl $@ ./scripts/system_appearance.ll >/dev/null 2>&1
-            echo "Launched Services"
-            break
-        fi
-        sleep 2
-    done
-    wait $PID
+CARGO_ARGS=""
+if [ "$MODE" = "live-on" ]; then
+    CARGO_ARGS="--release"
+fi
+
+echo "Mode: $MODE"
+
+while true; do
+     PID=$(RUST_LOG=debug cargo run --bin vrsd "$CARGO_ARGS" > vrsd.log) &
+
+     while true; do
+         cargo run --bin vrsctl "$CARGO_ARGS" ./scripts/launcher.ll >/dev/null 2>&1
+         if [ $? -eq 0 ]; then
+             cargo run --bin vrsctl "$CARGO_ARGS" ./scripts/chat.ll >/dev/null 2>&1
+             cargo run --bin vrsctl "$CARGO_ARGS" ./scripts/system_appearance.ll >/dev/null 2>&1
+
+             # Restart vrsjmp if live-on
+             if [ "$MODE" = "live-on" ]; then
+                 pkill -ax "vrsjmp"
+                 PID=$(RUST_LOG=debug cargo run --bin vrsjmp "$CARGO_ARGS" > /dev/null) &
+                 wait $PID
+             fi
+
+             echo "Launched Services"
+             break
+         fi
+         sleep 2
+     done
+     wait $PID
 done
