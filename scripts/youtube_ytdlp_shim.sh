98514934b24a35c2e5f63d00f2d6add2282a4b0a
leoshimo <56844000+leoshimo@users.noreply.github.com>
2024-12-31 00:01:41 -0500

    fix(youtube): Empty / Missing youtube directory shows bad entry

--- scripts/youtube_ytdlp_shim.sh
+++ scripts/youtube_ytdlp_shim.sh
@@ -6,12 +6,14 @@ set -euo pipefail
 
 # TODO: Improve ergonomics around defining shell scripts directly in lyric
 echo "("
-for channel_path in ~/Downloads/yt-dlp/*; do
-    channel_name=$(basename "$channel_path")
-    for video_path in "${channel_path}"/*; do
-        video_name=$(basename "$video_path")
-        video_webm_path="${video_path}/${video_name}.webm"
-        echo "(:title \"${channel_name} - ${video_name}\" :path \"$video_webm_path\")"
+if [ -d "~/Downloads/yt-dlp" ]; then
+    for channel_path in ~/Downloads/yt-dlp/*; do
+        channel_name=$(basename "$channel_path")
+        for video_path in "${channel_path}"/*; do
+            video_name=$(basename "$video_path")
+            video_webm_path="${video_path}/${video_name}.webm"
+            echo "(:title \"${channel_name} - ${video_name}\" :path \"$video_webm_path\")"
+        done
     done
-done
+fi
 echo ")"
