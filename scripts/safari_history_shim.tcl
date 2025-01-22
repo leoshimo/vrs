a90dfa423cbe7e5956a0d3b9a251d0e68d7b4681
leoshimo <56844000+leoshimo@users.noreply.github.com>
2025-01-21 17:09:22 -0800

    config(vrsjmp): Update Safari History window

--- scripts/safari_history_shim.tcl
+++ scripts/safari_history_shim.tcl
@@ -11,7 +11,7 @@ set output "(\n"
 catch {
     sqlite3 historydb "$env(HOME)/Library/Safari/History.db"
     historydb eval {
-        SELECT datetime(visit_time + 978307200, 'unixepoch', 'localtime') as local_visit_time, TRIM(title) as trim_title, url, domain_expansion
+        SELECT datetime(visit_time + 978307200, 'unixepoch', 'localtime') as local_visit_time, TRIM(title) as trim_title, MIN(url) as min_url, domain_expansion, *
         FROM history_visits
         JOIN history_items ON history_visits.history_item = history_items.id
         WHERE LENGTH(TRIM(COALESCE(title, ''))) > 0
@@ -19,11 +19,11 @@ catch {
             AND LENGTH(url) < 500
             AND url NOT LIKE "%/search%"
             AND url NOT LIKE "%read.amazon.co.jp%"
-        GROUP BY history_item
+        GROUP BY trim_title
         ORDER BY visit_time DESC, visit_count_score DESC
-        LIMIT 250
+        LIMIT 150
     } {
-        append output "(:safari_history_item :title \"$trim_title\" :url \"$url\" :domain_expansion \"$domain_expansion\")"
+        append output "(:safari_history_item :title \"$trim_title\" :url \"$min_url\" :domain_expansion \"$domain_expansion\")"
     }
 }
 
